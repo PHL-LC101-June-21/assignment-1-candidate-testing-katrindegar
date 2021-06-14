bcee204fb829a5779daa4b26a310ee73fb314b7a
katrindegar <84479109+katrindegar@users.noreply.github.com>
2021-06-14 16:01:36 +0000

    final 1.3

--- candidate-testing.js
+++ candidate-testing.js
@@ -18,7 +18,7 @@ candidateAnswer = " ";
 questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
 correctAnswers = ["Sally Ride", 'true', '40', "Trajectory", '3'];
 candidateAnswers = ["","","","",""];
-
+candidateName = "";
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
