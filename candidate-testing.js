ca97eaa094220846b2e15fb04c63e2e4c346b0cc
katrindegar <84479109+katrindegar@users.noreply.github.com>
2021-06-14 15:53:35 +0000

    final 1.1

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,12 +5,19 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";;
-let correctAnswer = "Sally Ride";;
-let candidateAnswer = " ";
-let questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
-let correctAnswers = ["Sally Ride", 'true', '40', "Trajectory", '3'];
-let candidateAnswers = ["","","","",""];
+let question;
+let correctAnswer;
+let candidateAnswer;
+let questions;
+let correctAnswers;
+let candidateAnswers;
+
+question = "Who was the first American woman in space? ";;
+correctAnswer = "Sally Ride";;
+candidateAnswer = " ";
+questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+correctAnswers = ["Sally Ride", 'true', '40', "Trajectory", '3'];
+candidateAnswers = ["","","","",""];
 
 
 function askForName() {
